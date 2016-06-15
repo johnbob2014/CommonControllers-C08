e9c0de04b1a74a7665300c0374a30676e9569eb7
johnbob2014 <johnbob2014@icloud.com>
2016-06-15 11:11:57 +0800

    Initial Commit

---
+++ CommonControllers-C08/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonControllers-C08
+//
+//  Created by BobZhang on 16/6/15.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
